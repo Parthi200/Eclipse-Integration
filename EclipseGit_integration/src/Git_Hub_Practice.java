0ef79a8e8a277c787c9c0682896e8aae3177c678
Parthi200 <thennavaiparthiban@gmail.com>
2024-08-25 11:40:04 +0530

    Git Repo

--- EclipseGit_integration/src/Git_Hub_Practice.java
+++ EclipseGit_integration/src/Git_Hub_Practice.java
@@ -3,7 +3,7 @@ public class Git_Hub_Practice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Git Repository");
+		System.out.println("24-08-2024");
 
 	}
 
